d537f0f4eeda85e7383db88add04d80e02ed7ae2
Cristino <cristino.lamadrid@xceldata.com>
2018-03-06 11:36:42 -0800

    Finished Lab 4.

--- lab4.py
+++ lab4.py
@@ -2,6 +2,9 @@
 
 import math
 import pygame
+import os
+import time
+import sys
 
 class Display:
    def __init__(self, frame_delay = 500):
@@ -25,56 +28,48 @@ class Display:
    
          fn()
          self.Screen.fill(self.BackgroundColor)
-         self.DrawDendrogram(cluster.Root, pygame.Rect(20, 40, 720, 540), 0.5)
+         self.DrawDendrogram(cluster.Root, pygame.Rect(20, 40, 720, 540))
          pygame.time.wait(self.FrameDelay)
          pygame.display.flip()
-
-   def DrawDendrogram(self, node, rect, justify):
+         if cluster.Finished():
+            if not os.path.isdir('lab4_images'):
+               os.mkdir('lab4_images')
+            pygame.image.save(self.Screen, 'lab4_images\lab4.png')
+            return
+      
+   def DrawDendrogram(self, node, rect):
       depth = node.Depth()
-      childrenCount = len(node.GetValues())
-      xmargin = 10
+      values = node.GetValues()
+      childrenCount = len(values)
 
-      midx = rect.left + (rect.width * justify)
-      starty = rect.top
-      endy = starty + (rect.height / depth)
-
-      flat_startx = rect.left + (xmargin * justify)
-      flat_width = rect.width - xmargin
-      flat_endx = flat_startx + flat_width
-
-      pygame.draw.lines(self.Screen, self.LineColor, False, [(midx, starty), (midx, endy)], 1);
-      if depth > 1:
-         pygame.draw.lines(self.Screen, self.LineColor, False, [(flat_startx, endy), (flat_endx, endy)], 1);
+      vertx = rect.left + (rect.width * 0.5)
+      vert_starty = rect.top
+      vert_endy = vert_starty + (rect.height / depth)
 
       consumedWidth = 0
+      childRects = []
       for i in range(len(node.Children)):
          childWeight = len(node.Children[i].GetValues())/childrenCount
          childRect = pygame.Rect(0, 0, 0, 0)
-         childRect.left = flat_startx + consumedWidth
-         childRect.width = flat_width * childWeight
-
-         childRect.top = endy
-         childRect.height = rect.top + rect.height - endy
-
-         childJustify = float(i) / (len(node.Children) - 1)
-         self.DrawDendrogram(node.Children[i], childRect, childJustify)
+         childRect.left = rect.left + consumedWidth
+         childRect.width = rect.width * childWeight
+         childRect.top = vert_endy
+         childRect.height = rect.top + rect.height - vert_endy
+         self.DrawDendrogram(node.Children[i], childRect)
          consumedWidth += childRect.width
+         childRects.append(childRect)
 
-   #def DrawPoints(self):
-   #   dw = 600
-   #   dh = 600
-   #   db = 10
-   #   offx = 170
-   #   offy = 10
-   #
-   #   pygame.draw.rect(self.Screen, (0x0, 0x0, 0x0), (offx, offy, dw + 2*db, dh + 2*db), 1)
-   #
-   #   for d in self.Points:
-   #      x = d.X
-   #      y = d.Y
-   #      color = (0xff, 0x0, 0x0)
-   #      pygame.draw.circle(self.Screen, color, (int(dw * x) + offx + db, int(dh * y) + offy + db), 5, 0)
-      
+      if len(childRects) > 1:
+         flat_startx = childRects[0].left + (childRects[0].width * 0.5)
+         flat_endx = childRects[-1].left + (childRects[-1].width * 0.5)
+         pygame.draw.lines(self.Screen, self.LineColor, False, [(flat_startx, vert_endy), (flat_endx, vert_endy)], 2)
+
+      if depth == 1:
+         text = self.Font.render("{:d}".format(values[0].Index), True, (0x0, 0x0, 0x0))
+         self.Screen.blit(text, (vertx - 3, vert_endy + 3, 0, 0))
+
+      pygame.draw.lines(self.Screen, self.LineColor, False, [(vertx, vert_starty), (vertx, vert_endy)], 2);
+
 class Node:
    def __init__(self, point = None):
       self.Value = point
@@ -107,6 +102,67 @@ class Node:
 
       return min_dist
 
+   def MaxDistance(self, node):
+      if not type(node) is Node:
+         raise TypeError("Not a node")
+
+      a_values = self.GetValues()
+      b_values = node.GetValues()
+
+      max_dist = -1
+      for a in a_values:
+         for b in b_values:
+            dist = a.Euclidean(b)
+            if dist > max_dist or max_dist < 0:
+               max_dist = dist
+
+      return max_dist
+
+   def GroupAverageDistance(self, node):
+      if not type(node) is Node:
+         raise TypeError("Not a node")
+
+      a_values = self.GetValues()
+      b_values = node.GetValues()
+
+      distances = []
+      for a in a_values:
+         for b in b_values:
+            distances.append(a.Euclidean(b))
+
+      group_average_dist = 0
+      for i in distances:
+         group_average_dist += i
+      group_average_dist /= len(distances)
+
+      return group_average_dist
+
+   def CentroidDistance(self, node):
+      if not type(node) is Node:
+         raise TypeError("Not a node")
+
+      a_values = self.GetValues()
+      b_values = node.GetValues()
+
+      a_centroid = Point(0, 0, 0)
+      a_centroid.X = 0
+      a_centroid.Y = 0
+      for a in a_values:
+         a_centroid.X += a.X
+         a_centroid.Y += a.Y
+      a_centroid.Y = a_centroid.Y / len(a_values)
+      
+      b_centroid = Point(0, 0, 0)
+      b_centroid.X = 0
+      b_centroid.Y = 0
+      for b in b_values:
+         b_centroid.X += b.X
+         b_centroid.Y += b.Y
+      b_centroid.X = b_centroid.X / len(b_values)
+      b_centroid.Y = b_centroid.Y / len(b_values)
+
+      return a_centroid.Euclidean(b_centroid)
+
    def Depth(self):
       if self.Value != None:
          return 1
@@ -119,7 +175,8 @@ class Node:
       return 1 + max_depth
 
 class Point:
-   def __init__(self, xabs, yabs):
+   def __init__(self, index, xabs, yabs):
+      self.Index = index
       self.XAbs = xabs
       self.YAbs = yabs
       self.X = 0
@@ -139,7 +196,7 @@ class Point:
 class Cluster:
    def __init__(self):
       self.Root = Node()
-      
+
    def Add(self, node):
       self.Root.AddChild(node)
       
@@ -163,7 +220,7 @@ class Cluster:
 
       for i in range(len(self.Root.Children)):
          for j in range(i + 1, len(self.Root.Children)):
-            self.Proximity[i][j] = self.Root.Children[i].MinDistance(self.Root.Children[j])
+            self.Proximity[i][j] = self.Root.Children[i].CentroidDistance(self.Root.Children[j])
 
    def PrintProximity(self):
       for i in range(len(self.Proximity)):
@@ -196,8 +253,11 @@ class Cluster:
    def Depth(self):
       return self.Root.Depth()
 
+   def Finished(self):
+      return True if len(self.Root.Children) <= 2 else False
+
    def Update(self):
-      if len(self.Root.Children) <= 2:
+      if self.Finished():
          return
 
       self.BuildMatrix()
@@ -209,13 +269,13 @@ if __name__ == '__main__':
    
    display = Display(500)
    dataCluster = Cluster()
-   any(dataCluster.Add(Node(Point(float(x[0]), float(x[1])))) for x in data)
+   any(dataCluster.Add(Node(Point(i, float(data[i][0]), float(data[i][1])))) for i in range(len(data)))
    dataCluster.Normalize()
 
    testCluster = Cluster()
-   testCluster.Add(Node(Point(0.25, 0.25)))
-   testCluster.Add(Node(Point(0.50, 0.50)))
-   testCluster.Add(Node(Point(0.75, 0.75)))
+   testCluster.Add(Node(Point(0, 0.25, 0.25)))
+   testCluster.Add(Node(Point(1, 0.50, 0.50)))
+   testCluster.Add(Node(Point(2, 0.75, 0.75)))
    testCluster.Normalize()
 
    display.Run(dataCluster, dataCluster.Update)
